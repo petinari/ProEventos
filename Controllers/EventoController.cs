37f442f21a0cc3c0824d866a211705a516c44aea
robsonpetinari <Petinari@outlook.com>
2023-03-22 14:09:21 -0400

    Inicial

---
+++ Controllers/EventoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EventoController : ControllerBase
+{
+  
+
+   
+
+    public EventoController()
+    {
+        
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<Evento> Get()
+    {
+
+    }
+}
+
